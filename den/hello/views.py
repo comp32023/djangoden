68df4e8a343366702c1ff45d4f4923720a13ce88
comp32023 <comp32023@mail.ru>
2024-01-26 16:56:23 +0300

    save1

---
+++ den/hello/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+
+
+def about(request):
+    return render(request, "comp1.html")
