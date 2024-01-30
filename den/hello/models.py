098ab023cdf58c37a1fa4da78a1c097fd96378c9
comp32023 <comp32023@mail.ru>
2024-01-30 13:19:46 +0300

    save3001

--- den/hello/models.py
+++ den/hello/models.py
@@ -1,9 +1,32 @@
 from django.db import models
 
-class compcena(models.Model):
-    id_cena = models.IntegerField(primary_key=True)
-    purchase_price = models.IntegerField()
-    price_for_sale = models.IntegerField()
-    date_of_purchase = models.DateField()
-    info = models.TextField(default=0)
-    info2 = models.IntegerField(default=0)
+class product_information(models.Model):
+    id_product = models.IntegerField(primary_key=True)
+    name_product = models.CharField(200)
+    category_id = models.IntegerField()
+    weight = models.TextField(default=0)
+    description = models.TextField(default=0)
+    structure_goods = models.TextField(default=0)
+    possibleboundaries = models.TextField(default=0)
+
+class categories(models.Model):
+    id_category = models.IntegerField(primary_key=True)
+    category_name = models.TextField()
+
+class sale(models.Model):
+    id_sale = models.IntegerField(primary_key=True)
+    name_sale = models.CharField(200)
+    price = models.IntegerField()
+    discounted_price = models.IntegerField(default=0)
+    purchase_price = models.IntegerField(default=0)
+    provider = models.TextField(default=0)
+    note = models.TextField(default=0)
+class orders(models.Model):
+    id_purchase = models.IntegerField(primary_key=True)
+    purchase = models.TextField()
+    price = models.IntegerField()
+    purchase_note = models.TextField(default=0)
+    date = models.IntegerField()
+    order_status = models.TextField() # статус заказа(в ожидании, ожидает оплату, подтверждён)
+
+
--- den/hello/views.py
+++ den/hello/views.py
@@ -1,23 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import compcena
+# from .models import compcena
 
 
 def index(request):
     return HttpResponse("<h2>Главная</h2>")
 
+# def vse(request):
+#     cena = compcena.objects.filter(id_cena=1)
+#     people = compcena.objects.all()
+#     data = {'people': people,'cena': cena}
+#     return render(request, "comp1.html", data)
+#
+# def about(request):
+#     cena = compcena.objects.filter(id_cena=1)
+#     data = {'cena': cena}
+#     return render(request, "comp1.html", data)
+#
+#
+# people = compcena.objects.all()
+# for person in people:
+#     print(f"{person.id_cena} {person.purchase_price} {person.price_for_sale}")
 
-def about(request):
-    cena = compcena.objects.filter(id_cena=1)
-    data = {'cena': cena}
-    return render(request, "comp1.html", data)
 
-def about2(request):
-    cena = compcena.objects.filter(id_cena=2)
-    data = {'cena': cena}
-    return render(request, "comp1.html", data)
 
-def about3(request):
-    cena = compcena.objects.filter(id_cena=3)
-    data = {'cena': cena}
-    return render(request, "comp1.html", data)
+# def about2(request):
+#     cena = compcena.objects.filter(id_cena=2)
+#     data = {'cena': cena}
+#     return render(request, "comp1.html", data)
+#
+# def about3(request,vse):
+#     sv = people
+#     cena = compcena.objects.filter(id_cena=3)
+#     data = {'cena': cena}
+#     return render(request, "comp1.html", data)
+
+
+
+
+# people = compcena.objects.all()
+# print(people.query)
+# sv = compcena.objects.filter(id_cena = 2)
+# print(sv)
+# test = people.filter(purchase_price = 2)
+# print(test.query)
+# tom = people.objects.get(purchase_price = 2)
